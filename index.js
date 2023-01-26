1057af999ed8a291c3d18a3163ae24b31860851a
Not TCA <realtcatech@gmail.com>
2023-01-26 16:45:57 +0800

    add 404 path

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.set("views", "./client/views");
 app.use("/", require("./client"));
 app.use("/api", require("./api"));
 
+app.get("*", (req, res, next) => {
+  if (req.path.startsWith("/api"))
+    res.status(404).json({
+      status: 404,
+      error: `The page ${req.path} doesn't exist.`,
+    });
+  else res.status(404).render("404");
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}.`);
